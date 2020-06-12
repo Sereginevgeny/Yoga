35541cd6e20a698e9420ae8b2d9b5939e941cf7b
Evgeny <sereginevgeny.96@gmail.com>
2020-06-12 16:03:38 +0300

    Добавлен слайдер, калькулятор и изменение в README

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-'use strict';
+ 'use strict';
 
 
 // находим селекторы
@@ -97,4 +97,104 @@ hideTabContent(1);
 				document.body.style.overflow = '';
 			});
 
+		// Слайдер
+
+		let slideIndex = 1,
+			 slides = document.querySelectorAll('.slider-item'),
+			 prev = document.querySelector('.prev'),
+			 next = document.querySelector('.next'),
+			 dotsWrap = document.querySelector('.slider-dots'),
+			 dots = document.querySelectorAll('.dot');
+
+			 
+		
+		function showSlides(n) {
+
+			if (n > slides.length) {
+				slideIndex = 1;
+			}
+			if (n < 1) {
+				slideIndex = slides.length;
+			}
+
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].style.display = 'none';
+			}
+			
+			// dots.forEach((item) => item.classList.remove('dot-active'));
+			for (let i = 0; i < dots.length; i++) {
+				dots[i].classList.remove('dot-active');
+			}
+	
+			slides[slideIndex - 1].style.display = "block";
+			dots[slideIndex - 1].classList.add('dot-active');
+		}
+		
+		showSlides(slideIndex);
+
+		function plusSlides(n) {
+			showSlides(slideIndex += n);
+		}
+		function currentSlide(n) {
+			showSlides(slideIndex = n);
+		}
+
+		prev.addEventListener('click', function() {
+			plusSlides(-1);
+		});
+		next.addEventListener('click', function() {
+			plusSlides(1);
+		});
+
+		dotsWrap.addEventListener('click', function(event) {
+			for (let i = 0; i < dots.length + 1; i++) {
+				if (event.target.clasList.contains('dot') && event.target == dots[i-1]) {
+					currentSlide(i);
+				}
+			}
+		});
+
+
+		//Калькулятор рассчитывает стоимость поездки
+
+		let persones = document.querySelectorAll('.counter-block-input')[0],
+			 restDays = document.querySelectorAll('.counter-block-input')[1],
+			 place = document.getElementById('select'),
+			 totalValue = document.getElementById('total'),
+			 personsSum = 0,
+			 daysSum = 0,
+			 total = 0;
+
+		totalValue.innerHTML = 0;
+
+		persones.addEventListener('change', function() {
+			personsSum = +this.value;
+			total = (daysSum + personsSum) * 4000;
+
+			if(restDays.value == '') {
+				totalValue.innerHTML = 0;
+			} else {
+				totalValue.innerHTML = total;
+			}
+		});
+
+		restDays.addEventListener('change', function() {
+			daysSum = +this.value;
+			total = (daysSum + personsSum) * 4000;
+
+			if(persones.value == '') {
+				totalValue.innerHTML = 0;
+			} else {
+				totalValue.innerHTML = total;
+			}
+		});
+
+		place.addEventListener('change', function() {
+			if (restDays.value == '' || persones.value == '') {
+				totalValue.innerHTML = 0;
+			} else {
+				let a = total;
+				totalValue.innerHTML = a * this.opion[this.selectedIndex].value;
+			}
+		});
 });
